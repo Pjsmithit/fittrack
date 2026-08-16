e52810b5735501fc9bd418ce80ff06d5f8d67aef
Pjsmithit <Info@pjsmithit.co.uk>
2026-08-16 21:15:01 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,35 @@
+import { route, notFound, startRouter, navigate } from "./router.js";
+import { seedExerciseLibraryIfNeeded } from "./exerciseLibrary.js";
+import { renderSetup } from "./views/setup.js";
+import { renderProgram } from "./views/program.js";
+import { renderDay } from "./views/day.js";
+import { renderExercise } from "./views/exerciseDetail.js";
+import { renderLog } from "./views/logging.js";
+import { renderProgress } from "./views/progress.js";
+
+route("/setup", renderSetup);
+route("/program", renderProgram);
+route("/day/:programId/:weekNumber/:dayNumber", renderDay);
+route("/exercise/:programId/:weekNumber/:dayNumber/:exerciseIndex", renderExercise);
+route("/log/:programId/:weekNumber/:dayNumber", renderLog);
+route("/progress", renderProgress);
+notFound(renderProgram);
+
+document.querySelectorAll(".tabbar button").forEach((btn) => {
+  btn.addEventListener("click", () => navigate(`/${btn.dataset.tab}`));
+});
+
+async function boot() {
+  await seedExerciseLibraryIfNeeded();
+  startRouter();
+}
+
+boot();
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("service-worker.js").catch(() => {
+      // Non-fatal: app still works, just without offline caching this session.
+    });
+  });
+}
